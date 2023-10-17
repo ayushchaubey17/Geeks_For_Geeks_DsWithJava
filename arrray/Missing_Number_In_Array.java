4247375335df8fc905d0227b11ab2ed0887633de
Ayush <ayushreso1234@gmail.com>
2023-10-17 22:57:19 +0530

    que 1 array

--- arrray/Missing_Number_In_Array.java
+++ arrray/Missing_Number_In_Array.java
@@ -1,3 +1,36 @@
+//Missing number in array
+
+//Given an array of size N-1 such that it only contains distinct integers in the range of 1 to N. Find the missing element.
+//
+//Example 1:
+//
+//Input:
+//N = 5
+//A[] = {1,2,3,5}
+//Output: 4
+//Example 2:
+//
+
+
+
+//Input:
+//N = 10
+//A[] = {6,1,2,8,3,4,7,10,5}
+//Output: 9
+//
+
+
+
+//Your Task :
+//You don't need to read input or print anything. Complete the function MissingNumber() that takes array and N as input  parameters and returns the value of the missing number.
+//
+//
+//Expected Time Complexity: O(N)
+//Expected Auxiliary Space: O(1)
+
+
+
+//public class
 public class Missing_Number_In_Array {
     public static void main(String[] args) {
         int arr []  = {1,2,3,8,4,5,6};
@@ -13,7 +46,7 @@ public class Missing_Number_In_Array {
 
 
 
-// User function Template for Java
+//Solution class
 
 class Solution {
     int MissingNumber(int array[] ,int n){
